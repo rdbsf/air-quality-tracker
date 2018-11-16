a1983c6d31b5a2b9aac52f60355024d5818417bb
ryanbishop <ryan.bishop@theelectronshop.com>
2018-11-15 22:10:38 -0800

    xml

--- routes.js
+++ routes.js
@@ -1,9 +1,28 @@
+var http = require('http');
+
 module.exports = function(app){
     //var mixes = require('./controllers/mixes');
 
     app.get('/', function(req, res){
-        res.setHeader('Content-Type', 'application/json');
-        res.write(JSON.stringify({'airquality': '1.0'}));  
+
+        http.get('http://feeds.enviroflash.info/rss/realtime/133.xml', function(res) {
+            var response_data = '';
+            res.setEncoding('utf8');
+            res.on('data', function(chunk) {
+                response_data += chunk;
+            });
+            res.on('end', function() {
+                //callback(null, response_data)
+                res.setHeader('Content-Type', 'application/json');
+                //res.write(JSON.stringify({'airquality': '1.0'}));  
+                res.write(response_data);  
+            });
+            res.on('error', function(err) {
+                callback(err);
+            });
+        });
+
+        
     });
     //app.get('/latest', mixes.findMostRecent);
     //app.get('/random', mixes.findRandom);

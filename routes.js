49db804344837644e0a525439d54408dd44963c6
ryanbishop <ryan.bishop@theelectronshop.com>
2018-11-16 16:39:22 -0800

    xml reading

--- routes.js
+++ routes.js
@@ -6,14 +6,26 @@ module.exports = function(app){
     app.get('/', function(req, res){
 
         request('http://feeds.enviroflash.info/rss/realtime/133.xml', function(error, response, body) {
-            res.setHeader('Content-Type', 'application/json');
+                //res.setHeader('Content-Type', 'application/json');
                 //res.write(JSON.stringify({'airquality': '1.0'}));  
+                // var parseString = require('xml2js').parseString;
+                // var xml = body;//"<root>Hello xml2js!</root>"
+                // parseString(xml, function (err, result) {
+                //     //console.dir(result);
+                //     res.write(result);  
+                // });
+
                 var parseString = require('xml2js').parseString;
-                var xml = body;//"<root>Hello xml2js!</root>"
+                var xml = body; //"<root>Hello xml2js!</root>"
                 parseString(xml, function (err, result) {
-                    //console.dir(result);
-                    res.write(result);  
+                    //res.setHeader('Content-Type', 'application/json');
+                    res.setHeader('Content-Type', 'text/html');
+                    let airDescription = result.rss.channel[0].item[0].description[0];
+                    //res.write(JSON.stringify({'air': airDescription}));  
+                    res.write(airDescription);  
                 });
+
+                
            
         });
 

751e71ba5d3653e14fa286f1be948de5e4b119c0
ryanbishop <ryan.bishop@theelectronshop.com>
2018-11-16 21:54:15 -0800

    promises promises

---
+++ feed.js
@@ -0,0 +1,44 @@
+
+var request = require('request');
+const cheerio = require('cheerio');
+
+module.exports = function(){
+    // Setting URL and headers for request
+    var options = {
+        url: 'http://feeds.enviroflash.info/rss/realtime/133.xml',
+        headers: {
+            'User-Agent': 'request'
+        }
+    };
+    // Return new promise 
+    return new Promise(function(resolve, reject) {
+        
+        request(options, function(error, response, body) {
+
+                if (error) {
+                    reject(err);
+                } else {
+
+                    var parseString = require('xml2js').parseString;
+                    var xml = body; 
+                    parseString(xml, function (err, result) {
+
+                        let airDescription = result.rss.channel[0].item[0].description[0];
+
+                        const $ = cheerio.load(airDescription)
+                        let qualityHtml = $('div table tbody tr:nth-child(2) td div:nth-child(3) div').html();
+                        let qualityMessage = qualityHtml.split('-')[0].trim();
+                        let aqi = qualityHtml.split('-')[1].trim().split(' ')[0];
+                        resolve({ airDescription: airDescription, message: qualityMessage, aqi: aqi });
+
+                    });
+
+
+                }
+        });
+
+    })
+
+}
+
+//module.exports.method = initialize;
--- routes.js
+++ routes.js
@@ -1,78 +1,48 @@
 var request = require('request');
-const cheerio = require('cheerio');
-
+var feed = require('./feed')
 module.exports = function(app){
  
     app.get('/', function(req, res){
 
-        request('http://feeds.enviroflash.info/rss/realtime/133.xml', function(error, response, body) {
+        var initializePromise = feed();
+        initializePromise.then(function(feedDetails) {
 
-                var parseString = require('xml2js').parseString;
-                var xml = body; 
-                parseString(xml, function (err, result) {
+            let qualityRangeNew = [
+                {'max': 50, 'class': 'good'},
+                {'max': 100, 'class':  'moderate'},
+                {'max': 150, 'class':  'unheathy1'},
+                {'max': 200, 'class':  'unheathy2'},
+                {'max': 300, 'class':  'veryunheatlhy'},
+                {'max': 1000, 'class':  'hazardous'}
+            ];
 
-                    let airDescription = result.rss.channel[0].item[0].description[0];
+            let qualityClass = 'good';
 
-                    let qualityRangeNew = [
-                        {'max': 50, 'class': 'good'},
-                        {'max': 100, 'class':  'moderate'},
-                        {'max': 150, 'class':  'unheathy1'},
-                        {'max': 200, 'class':  'unheathy2'},
-                        {'max': 300, 'class':  'veryunheatlhy'},
-                        {'max': 1000, 'class':  'hazardous'}
-                    ];
+            let rangeFilter = qualityRangeNew.filter(val => {
+                return val.max >= parseInt(feedDetails.aqi, 10);
+            });
 
-                    const $ = cheerio.load(airDescription)
-                    let qualityHtml = $('div table tbody tr:nth-child(2) td div:nth-child(3) div').html();
-                    let qualityMessage = qualityHtml.split('-')[0].trim();
-                    let aqi = qualityHtml.split('-')[1].trim().split(' ')[0];
+            qualityClass = rangeFilter.shift().class;
 
-                    let qualityClass = 'good';
+            res.render('index', { title: 'SF Air Quality', airDescription: feedDetails.airDescription, qualityClass: qualityClass, message: feedDetails.message, aqi: feedDetails.aqi })
 
-                    let rangeFilter = qualityRangeNew.filter(val => {
-                        return val.max >= parseInt(aqi, 10);
-                    });
-
-                    qualityClass = rangeFilter.shift().class;
-
-                    res.render('index', { title: 'SF Air Quality', airDescription: airDescription, qualityClass: qualityClass, message: qualityMessage, aqi: aqi })
-
-                });
-
-            
-        });
+        }, function(err) {
+            console.log(err);
+        })
 
 
     });
 
     app.get('/aqi', function(req, res){
-        request('http://feeds.enviroflash.info/rss/realtime/133.xml', function(error, response, body) {
 
-                var parseString = require('xml2js').parseString;
-                var xml = body; 
-                parseString(xml, function (err, result) {
-                
-                    let airDescription = result.rss.channel[0].item[0].description[0];
-
-                    let qualityRangeNew = [
-                        {'max': 50, 'class': 'good'},
-                        {'max': 100, 'class':  'moderate'},
-                        {'max': 150, 'class':  'unheathy1'},
-                        {'max': 200, 'class':  'unheathy2'},
-                        {'max': 300, 'class':  'veryunheatlhy'},
-                        {'max': 1000, 'class':  'hazardous'}
-                    ];
-
-                    const $ = cheerio.load(airDescription)
-                    let qualityHtml = $('div table tbody tr:nth-child(2) td div:nth-child(3) div').html();
-                    let qualityMessage = qualityHtml.split('-')[0].trim();
-                    let aqi = qualityHtml.split('-')[1].trim().split(' ')[0];
-                    
-                    res.setHeader('Content-Type', 'application/json');
-                    res.write(JSON.stringify({'aqi': aqi}));  
-                });
+        var initializePromise = feed();
+        initializePromise.then(function(result) {
+            feedDetails = result;
+            res.setHeader('Content-Type', 'application/json');
+            res.write(JSON.stringify({'aqi': feedDetails.aqi}));  
 
         });
+
     });
 
   }

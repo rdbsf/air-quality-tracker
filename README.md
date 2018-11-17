626e1da35f2faaf62a52bd10f1628a897bf375e5
ryanbishop <ryan.bishop@theelectronshop.com>
2018-11-16 18:11:37 -0800

    updates

--- README.md
+++ README.md
@@ -8,3 +8,7 @@ Heroku
 git push heroku master
 heroku open
 heroku logs --tail
+
+
+clouds
+https://codepen.io/P3R0/pen/RPbgaX
--- routes.js
+++ routes.js
@@ -7,52 +7,53 @@ module.exports = function(app){
     app.get('/', function(req, res){
 
         request('http://feeds.enviroflash.info/rss/realtime/133.xml', function(error, response, body) {
-                //res.setHeader('Content-Type', 'application/json');
-                //res.write(JSON.stringify({'airquality': '1.0'}));  
-                // var parseString = require('xml2js').parseString;
-                // var xml = body;//"<root>Hello xml2js!</root>"
-                // parseString(xml, function (err, result) {
-                //     //console.dir(result);
-                //     res.write(result);  
-                // });
 
                 var parseString = require('xml2js').parseString;
                 var xml = body; //"<root>Hello xml2js!</root>"
                 parseString(xml, function (err, result) {
-                    //res.setHeader('Content-Type', 'application/json');
-                    //res.setHeader('Content-Type', 'text/html');
+
                     let airDescription = result.rss.channel[0].item[0].description[0];
-                    //res.write(JSON.stringify({'air': airDescription}));  
-                    //res.write(airDescription);  
-const rangeGoodMax = 50;
-const rangeModerateMax = 100;
-const rangeUnhealthyMax = 200;
-const rangeVeryunhealthyMax = 300;
+    
+                    const rangeGoodMax = 50;
+                    const rangeModerateMax = 100;
+                    const rangeUnhealthy1Max = 150;
+                    const rangeUnhealthyMax = 200;
+                    const rangeVeryunhealthyMax = 300;
 
-const $ = cheerio.load(airDescription)
-let qualityHtml = $('div table tbody tr:nth-child(2) td div:nth-child(3) div').html();
-let qualityMessage = qualityHtml.split('-')[0].trim();
-let aqi = qualityHtml.split('-')[1].trim().split(' ');
-
-
-res.render('index', { title: 'SF Air Quality', airDescription: airDescription, qualityClass: 'veryunheatlhy', message: qualityMessage, aqi: aqi[0] })
-
-                    // parseString(airDescription.trim(), function (err, result) {
-
-                    //     res.render('index', { title: 'SF Air Quality', message: result })
-
-                    // });
-
+                    let qualityRange = [50,100,150,200,300];
+                    let qualityClasses = ['good', 'moderate', 'unheathy1', 'unheathy2', 'hazardous'];
                     
+                    let qualityRangeNew = [
+                        {'max': 50, 'class': 'good'},
+                        {'max': 100, 'class':  'moderate'},
+                        {'max': 150, 'class':  'unheathy1'},
+                        {'max': 200, 'class':  'unheathy2'},
+                        {'max': 300, 'class':  'veryunheatlhy'},
+                        {'max': 1000, 'class':  'hazardous'}
+                    ];
+
+                    const $ = cheerio.load(airDescription)
+                    let qualityHtml = $('div table tbody tr:nth-child(2) td div:nth-child(3) div').html();
+                    let qualityMessage = qualityHtml.split('-')[0].trim();
+                    let aqi = qualityHtml.split('-')[1].trim().split(' ');
+
+                    let qualityClass = 'good';
+
+                    let rangeFilter = qualityRangeNew.filter(val => {
+                        return val.max >= parseInt(aqi, 10);
+                    });
+
+                    qualityClass = rangeFilter.shift().class;
+
+                    res.render('index', { title: 'SF Air Quality', airDescription: airDescription, qualityClass: qualityClass, message: qualityMessage, aqi: aqi[0] })
+
                 });
 
-                
-           
+            
         });
 
-        
+
     });
-    //app.get('/latest', mixes.findMostRecent);
-    //app.get('/random', mixes.findRandom);
+
   }
   
